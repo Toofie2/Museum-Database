30c780892f9a29b97039060c388260c8bdb9912d
Isaias Gonzalez <xisaiasgonzalez@gmail.com>
2024-09-29 14:55:43 -0500

    Initialize project

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes that 
+const employeesRouter = require('./routes/employees');
+app.use('/employees', employeesRouter);
+
+
+app.get("/", (req, res) => {
+    res.json("Hello, this is the backend!");
+});
+
+app.listen(port, () => {
+    console.log(`Connected to backend on port ${port}`);
+});
