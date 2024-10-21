ded52fbe0682303b7cd2fa1f4de3926ef14060f9
Isaias Gonzalez <xisaiasgonzalez@gmail.com>
2024-10-20 21:58:30 -0500

    Fixed employee attributes and changed name to singular

--- server/server.js
+++ server/server.js
@@ -7,9 +7,9 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes that 
-const employeesRouter = require('./routes/employees');
-app.use('/employees', employeesRouter);
+// Backend routes for each table
+const employeeRouter = require('./routes/employee');
+app.use('/employee', employeeRouter);
 
 const customer_exhibitionRouter = require('./routes/customer_exhibition');
 app.use('/customer_exhibition', customer_exhibitionRouter);
