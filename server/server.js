73db1961fe9630a27ae870f6891c09d77fa7f9de
JosephOnwukwe <159090955+JosephOnwukwe@users.noreply.github.com.>
2024-10-20 20:47:05 -0500

    added files for tables

--- server/server.js
+++ server/server.js
@@ -14,7 +14,14 @@ app.use('/employees', employeesRouter);
 const ticketRouter = require('./routes/ticket');
 app.use('/ticket', ticketRouter);
 
+const customerTicketRouter = require('./routes/customer-ticket.js');
+app.use('/customer-ticket', customerTicketRouter);
 
+const productRouter = require('./routes/product');
+app.use('/product', productRouter);
+
+const customerProductRouter = require('./routes/customer-product.js');
+app.use('/customer-product', customerProductRouter);
 
 app.get("/", (req, res) => {
     res.json("Hello, this is the backend!");
