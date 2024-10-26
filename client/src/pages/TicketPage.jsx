6e49130b6fbde139f76a27f8e5a8fb947065059f
JosephOnwukwe <159090955+JosephOnwukwe@users.noreply.github.com.>
2024-10-26 15:03:57 -0500

    VERY primitive Ticket Page

--- client/src/pages/TicketPage.jsx
+++ client/src/pages/TicketPage.jsx
@@ -2,6 +2,42 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 
 const TicketPage = () => {
+    const[tickets, setTickets] = useState([])
+    useEffect(() => {
+        const fetchAllTickets = async()=>{
+            try{
+                const res = await axios.get('http://localhost:3000/ticket')
+                setTickets(res.data);
+            }catch(err){
+                console.log(err)
+            }
+        }
+        fetchAllTickets()
+    }, [])
+    return (
+        <div>
+            <h1>Purchase</h1>
+            <p>Please select an available date for your visit and the amount of tickets you’ll be purchasing.</p>
+            <div className="tickets">
+                {tickets.map(ticket=>(
+                    <div className="ticket" key={ticket.ticket_id}>
+                        <p class="capitalize">{ticket.type} admission <span> ${ticket.price}</span><span><button>-</button><button>+</button></span></p>
+                    </div>
+                ))}
+            </div>
+            <div>
+                <h2>Subtotal</h2>
+                <button>Checkout</button>
+            </div>
+        </div>
+    );
+};
+
+
+
+
+
+/*const TicketPage = () => {
     const[data, setData] = useState([])
     useEffect(() => {
         axios.get('http://localhost:3000/ticket')
@@ -39,6 +75,6 @@ const TicketPage = () => {
             </div>
         </div>
     );
-};
+};*/
 
 export default TicketPage;
