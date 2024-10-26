c1fec6179d074f8da30f308d3413f04275da41be
JosephOnwukwe <159090955+JosephOnwukwe@users.noreply.github.com.>
2024-10-26 02:37:23 -0500

    everything should be synced with main now

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,18 @@
+import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter as Router } from "react-router-dom";
+//import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  /*<StrictMode>
     <Router>
       <App />
     </Router>
+  </StrictMode>,*/
+
+  <StrictMode>
+    <App />
   </StrictMode>,
 );
