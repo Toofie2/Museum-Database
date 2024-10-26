6b7091d2a48916efbf8255bb5d4fad8831048c39
JosephOnwukwe <159090955+JosephOnwukwe@users.noreply.github.com.>
2024-10-26 15:44:21 -0500

    implementing adding and subtracting tickets

--- client/src/pages/TicketPage.jsx
+++ client/src/pages/TicketPage.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 const TicketPage = () => {
     const[tickets, setTickets] = useState([])
+    const[Subtotal, setSubtotal] = useState(0)
     useEffect(() => {
         const fetchAllTickets = async()=>{
             try{
@@ -30,51 +31,25 @@ const TicketPage = () => {
                 <button>Checkout</button>
             </div>
         </div>
+        /*
+        <div>
+            <h1>Purchase</h1>
+            <p>Please select an available date for your visit and the amount of tickets you’ll be purchasing.</p>
+            <div className="tickets">
+                {tickets.map(ticket=>(
+                    <div className="ticket" key={ticket.ticket_id}>
+                        <p class="capitalize">{ticket.type} admission <span> ${ticket.price}</span><span><button>-</button><button>+</button></span></p>
+                    </div>
+                ))}
+            </div>
+            <div>
+                <h2>Subtotal</h2>
+                <button>Checkout</button>
+            </div>
+        </div>*/    
     );
 };
 
 
-
-
-
-/*const TicketPage = () => {
-    const[data, setData] = useState([])
-    useEffect(() => {
-        axios.get('http://localhost:3000/ticket')
-        .then(res => setData(res.data))
-        .catch(err => console.log(err));
-    }, [])
-    return (
-        <div>
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>ticket_id</th>
-                            <th>type</th>
-                            <th>price</th>
-                            <th>is_deleted</th>
-                            <th>action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((ticket, index) => {
-                            return <tr key = {index}>
-                                <td>{ticket.ticket_id}</td>
-                                <td>{ticket.type}</td> 
-                                <td>{ticket.price}</td>
-                                <td>{ticket.is_deleted}</td>
-                                <td>
-                                    <button>Edit</button>
-                                    <button>Delete</button>
-                                </td>     
-                            </tr>
-                        })}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
-};*/
 
 export default TicketPage;
