c226e9afc5705eca1cb9e95e90a47c9180d9651e
JosephOnwukwe <159090955+JosephOnwukwe@users.noreply.github.com>
2024-10-27 04:01:10 -0500

    moved contents of TicketPage to TicketForm. Added TicketInfo

--- client/src/components/TicketInfo.jsx
+++ client/src/components/TicketInfo.jsx
@@ -4,11 +4,22 @@ const TicketInfo = () => {
     return(
        <div>
             <h1 class="text-2xl font-medium">
-                Purchase
+                Admission Tickets
             </h1>
             <p>
-                Please select an available date for your visit and the amount of tickets you’ll be purchasing.
+                $30 for adults; $22 for seniors; $17 for students; <br></br>
+                Free for Members, children under 12, veterans, and a caregiver accompanying a visitor with a disability. <br></br> 
+                Must verify student or veteran status at entry with a valid ID <br></br>
+                All tickets give access to all exhibitions and same-day entry to museum for the date on your ticket.
             </p>
+            <div class="text-white box-border h-full w-96 p-4 border-4 border-gray-brown rounded bg-gray-brown">
+                <h2 class="text-xl font-medium">
+                    Become a Member
+                </h2>
+                <p>
+                    Enjoy <span class="font-medium">unlimited free admission</span> for you and your guest(s) on every visit!
+                </p>
+            </div>
        </div> 
     )
 }
