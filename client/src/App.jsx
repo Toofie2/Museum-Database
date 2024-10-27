98eaebc873b269d9f17e135444b7ab6cd5d83e93
Toofie2 <ttiplee2@aol.com>
2024-10-27 18:50:43 -0500

    added Exhibitions Page- gets current exhibitions and routes user to the respective exhibitions views. added Exhibitions View Page- Displays selected exhibition with description and shows 4 art pieces from the database. added Route in art.js- gets art data from the database that matches exhibit_id for Exhibitions View Page. added url route to App.js- to direct the webpage.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
+import ExhibitionsPage from './pages/ExhibitionsPage';
+import ExhibitionViewPage from './pages/ExhibitionViewPage';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/exhibitions" element={<ExhibitionsPage />} />
+      <Route path="/exhibition/:id" element={<ExhibitionViewPage />} />
     </Routes>
   );
 };
