74fe774820b56e8751a60dd6b8d27a16a975f3e1
JosephOnwukwe <159090955+JosephOnwukwe@users.noreply.github.com.>
2024-10-27 15:20:49 -0500

    TicketPage is mostly styled. can't focus too long on it. need to work on gift shop

--- client/src/components/TicketForm.jsx
+++ client/src/components/TicketForm.jsx
@@ -30,6 +30,7 @@ const TicketForm = () => {
         }))
     }
 
+
     const handleSubtotal = (formData)=>{
         let ans = formData.adult * ticketPrices.at(0) + formData.senior * ticketPrices.at(1) + formData.youth * ticketPrices.at(2) + formData.child * ticketPrices.at(3) + formData.student * ticketPrices.at(4);
         setSubtotal(ans);
@@ -64,6 +65,11 @@ const TicketForm = () => {
             for(let j = 0; j < formData.student; j++){
                 promises.push(axios.post("http://localhost:3000/customer_ticket", purchasedTicket));
             }
+            purchasedTicket.ticket_id = ticketIDs.at(5);
+            purchasedTicket.amount_spent = ticketPrices.at(5);
+            for(let j = 0; j < formData.veteran; j++){
+                promises.push(axios.post("http://localhost:3000/customer_ticket", purchasedTicket));
+            }
             let allPromises = Promise.all(promises).then(() => console.log('all done'));
         }catch(err){
             console.log(err)
@@ -95,70 +101,102 @@ const TicketForm = () => {
         <div className="ticketForm">
             <h1 class="text-3xl font-medium">Purchase</h1>
             <p class="mt-3 leading-loose">Please select an available date for your visit and the amount of tickets you’ll be purchasing.</p>
-            <form>
-                <label>
-                    <span class="font-medium">Adult Admission (19+)</span> ${ticketPrices.at(0)} &emsp;&emsp;
+            <form class="mt-6">
+                <span >
+                <label class="flex justify-between">
+                    <div>
+                    <span class="font-medium">Adult Admission (19+)</span> ${ticketPrices.at(0)}
+                    </div>
                     <input 
                         type="number"
                         name="adult"
+                        min="0"
+                        max="20"
                         value={formData.adult}
                         onChange={handleChange} 
+                        class="w-12"
                     />
                 </label>
                 <br></br>
-                <label>
-                    <span class="font-medium">Senior Admission (65+)</span>  ${ticketPrices.at(1)} &emsp;&emsp;
+                <label class="flex justify-between">
+                    <div>
+                    <span class="font-medium">Senior Admission (65+)</span>  ${ticketPrices.at(1)}
+                    </div>
                     <input 
                         type="number"
                         name="senior"
+                        min="0"
+                        max="20"
                         value={formData.senior}
                         onChange={handleChange} 
+                        class="w-12"
                     />
                 </label>
                 <br></br>
-                <label>
-                <span class="font-medium">Youth Admission (13-18)</span>  ${ticketPrices.at(2)} &emsp;&emsp;
+                <label class="flex justify-between">
+                    <div>
+                <span class="font-medium">Youth Admission (13-18)</span>  ${ticketPrices.at(2)}
+                </div>
                     <input 
                         type="number"
                         name="youth"
+                        min="0"
+                        max="20"
                         value={formData.youth}
-                        onChange={handleChange} 
+                        onChange={handleChange}
+                        class="w-12" 
                     />
                 </label>
                 <br></br>
-                <label>
-                <span class="font-medium">Child Admission (12 & Under)</span>  ${ticketPrices.at(3)} &emsp;&emsp;
+                <label class="flex justify-between">
+                    <div>
+                <span class="font-medium">Child Admission (12 & Under)</span>  ${ticketPrices.at(3)}
+                </div>
                     <input 
                         type="number"
                         name="child"
+                        min="0"
+                        max="20"
                         value={formData.child}
-                        onChange={handleChange} 
+                        onChange={handleChange}
+                        class="w-12" 
                     />
                 </label>
                 <br></br>
-                <label>
-                <span class="font-medium">Student Admission (with valid ID)</span>  ${ticketPrices.at(4)} &emsp;&emsp;
+                <label class="flex justify-between">
+                    <div>
+                <span class="font-medium">Student Admission (with valid ID)</span>  ${ticketPrices.at(4)}
+                </div>
                     <input 
                         type="number"
                         name="student"
+                        min="0"
+                        max="20"
                         value={formData.student}
-                        onChange={handleChange} 
+                        onChange={handleChange}
+                        class="w-12" 
                     />
                 </label>
                 <br></br>
-                <label>
-                <span class="font-medium">Veteran Admission (with valid ID)</span>  ${ticketPrices.at(5)} &emsp;&emsp;
+                <label class="flex justify-between">
+                    <div>
+                <span class="font-medium">Veteran Admission (with valid ID)</span>  ${ticketPrices.at(5)}
+                </div>
                     <input 
                         type="number"
                         name="veteran"
+                        min="0"
+                        max="20"
                         value={formData.veteran}
-                        onChange={handleChange} 
+                        onChange={handleChange}
+                        class="w-12" 
                     />
                 </label>
-                <div>
-                    <span class="text-default-gray">Subtotal${subtotal}</span>
+                </span>
+                <div class="mt-10 text-default-gray text-lg flex justify-between">
+                    <span class="font-medium">Subtotal</span>${subtotal}
                 </div>
-                <button class="bg-black text-white py-2 px-52 border-black rounded" onClick={handleSubmit}>
+                <button class="w-full mt-4 bg-black text-white py-2 px-52 border-black rounded" onClick={handleSubmit}>
                     Checkout
                 </button>   
             </form>
