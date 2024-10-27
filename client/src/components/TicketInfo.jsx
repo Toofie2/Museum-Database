05da9e20024d7428b798275b5da8326d7d0143bc
JosephOnwukwe <159090955+JosephOnwukwe@users.noreply.github.com>
2024-10-27 03:10:38 -0500

    created ticket info

---
+++ client/src/components/TicketInfo.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const TicketInfo = () => {
+    return(
+       <div>
+            <h1 class="text-2xl font-medium">
+                Purchase
+            </h1>
+            <p>
+                Please select an available date for your visit and the amount of tickets you’ll be purchasing.
+            </p>
+       </div> 
+    )
+}
+
+
+
+
+export default TicketInfo
