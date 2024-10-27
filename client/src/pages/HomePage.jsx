1f91a0d58970f0a75fb66f840b265521850af874
Isaias Gonzalez <xisaiasgonzalez@gmail.com>
2024-10-26 20:52:34 -0500

    Page reference (incomplete)

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,15 @@
+import Navbar from "../components/Navbar.jsx";
+import backgroundImage from "../assets/HomePageBackground.jpg";
+
 const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <div className="relative min-h-screen">
+            <Navbar />
+            <div className="absolute inset-0 bg-black/50" />
+            <div className="min-h-screen bg-[url('../assets/HomePageBackground.jpg')] bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${backgroundImage})` }}>
+                <p className="text-white text-4xl">The Fine Arts Museum</p>
+            </div>
+        </div>
     );
 };
 
