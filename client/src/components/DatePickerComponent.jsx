daf242eb616afda884578f2e6baf8557b14cad2b
JosephOnwukwe <159090955+JosephOnwukwe@users.noreply.github.com.>
2024-10-28 15:50:00 -0500

    date selector nearly finished

--- client/src/components/DatePickerComponent.jsx
+++ client/src/components/DatePickerComponent.jsx
@@ -5,22 +5,36 @@ import { useState } from "react";
 
 const DatePickerComponent = () =>{
 
+    function addMonths(date, months) {
+        let temp = new Date(date)
+        temp.setMonth(temp.getMonth() + months)
+        if (temp.getDate() != date.getDate()) temp.setDate(0)
+        return temp
+      }
+
     const [selectedDate, setSelectedDate] = useState(new Date())
+    const minDate = new Date();
+    const maxDate = addMonths(minDate, 3);
 
     const handleDateChange = (date) => {
         setSelectedDate(date);
     };
 
-    return(
+
+    return{
+        selectedDate,
+        render:(
         <div>
             <DatePicker 
             selected={selectedDate}
             onChange={handleDateChange}
-            dateFormat="MMMM dd, YYYY"
+            dateFormat="MMMM dd, yyyy"
+            minDate={minDate}
+            maxDate={maxDate}
             />
         </div>
-
-    )
+        )
+    }
 }
 
 export default DatePickerComponent;
