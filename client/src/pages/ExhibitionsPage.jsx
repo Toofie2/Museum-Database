a590313a3715fa99989eca53098a929037032316
Toofie2 <ttiplee2@aol.com>
2024-10-27 20:33:21 -0500

    Added display end_date and ending soon functionality.

--- client/src/pages/ExhibitionsPage.jsx
+++ client/src/pages/ExhibitionsPage.jsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar";
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const ExhibitionsPage = () => {
   const [Exhibitions, setExhibitions] = useState([]);
@@ -9,12 +9,14 @@ const ExhibitionsPage = () => {
   useEffect(() => {
     const fetchExhibitions = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/exhibition');
+        const response = await axios.get("http://localhost:3000/exhibition");
         // Sort exhibitions by end date
-        const sortedExhibitions = response.data.sort((a, b) => new Date(a.end_date) - new Date(b.end_date));
+        const sortedExhibitions = response.data.sort(
+          (a, b) => new Date(a.end_date) - new Date(b.end_date)
+        );
         setExhibitions(sortedExhibitions);
       } catch (error) {
-        console.error('Error fetching exhibitions:', error);
+        console.error("Error fetching exhibitions:", error);
       }
     };
     fetchExhibitions();
@@ -22,7 +24,7 @@ const ExhibitionsPage = () => {
 
   // Function to format date and check if it's ending soon
   const formatEndDate = (dateString) => {
-    const options = { month: 'long', day: 'numeric', year: 'numeric' };
+    const options = { month: "long", day: "numeric", year: "numeric" };
     const endDate = new Date(dateString);
     const today = new Date();
 
@@ -31,9 +33,9 @@ const ExhibitionsPage = () => {
     const daysRemaining = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
     const isEndingSoon = daysRemaining <= 30 && daysRemaining >= 0;
 
-    return { 
-      formattedDate: `Ends ${endDate.toLocaleDateString('en-US', options)}`, 
-      isEndingSoon 
+    return {
+      formattedDate: `Ends ${endDate.toLocaleDateString("en-US", options)}`,
+      isEndingSoon,
     };
   };
 
@@ -48,9 +50,13 @@ const ExhibitionsPage = () => {
           className="object-cover absolute top-0 left-0 h-full w-screen"
         />
         <div className="absolute inset-0 bg-black bg-opacity-40 flex flex-col justify-center pl-10 space-y-2">
-          <h1 className="text-8xl font-regular text-white z-10 w-[600px] p-6">Exhibitions</h1>
+          <h1 className="text-8xl font-regular text-white z-10 w-[600px] p-6">
+            Exhibitions
+          </h1>
           <p className="text-3xl font-thin text-white z-10 w-[800px] p-10">
-            Explore our diverse exhibitions, from stunning paintings to captivating illustrations, thought-provoking installations to beautiful sculptures.
+            Explore our diverse exhibitions, from stunning paintings to
+            captivating illustrations, thought-provoking installations to
+            beautiful sculptures.
           </p>
         </div>
       </div>
@@ -59,9 +65,14 @@ const ExhibitionsPage = () => {
       <div className="container mx-auto py-10 pb-16">
         <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-10">
           {Exhibitions.map((Exhibition) => {
-            const { formattedDate, isEndingSoon } = formatEndDate(Exhibition.end_date);
+            const { formattedDate, isEndingSoon } = formatEndDate(
+              Exhibition.end_date
+            );
             return (
-              <div key={Exhibition.exhibit_id} className="exhibition-item relative bg-white rounded-lg overflow-hidden shadow-lg transition-transform duration-300 transform hover:scale-105 w-auto">
+              <div
+                key={Exhibition.exhibit_id}
+                className="exhibition-item relative bg-white rounded-lg overflow-hidden shadow-lg transition-transform duration-300 transform hover:scale-105 w-auto"
+              >
                 <Link to={`/exhibition/${Exhibition.exhibit_id}`}>
                   <img
                     src={Exhibition.image_path}
@@ -70,7 +81,9 @@ const ExhibitionsPage = () => {
                   />
                   <div className="absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-t from-black to-transparent opacity-50"></div>
                   <div className="absolute inset-0 bg-black bg-opacity-0 transition-opacity duration-300 ease-in-out hover:bg-opacity-40 flex items-end">
-                    <h2 className="text-2xl font-thin text-white ml-4 mb-4">{Exhibition.name}</h2>
+                    <h2 className="text-2xl font-thin text-white ml-4 mb-4">
+                      {Exhibition.name}
+                    </h2>
                   </div>
                   {/* End Date Text */}
                   <div className="text-sm absolute top-0 left-0 m-4 text-white bg-black bg-opacity-60 px-2 py-1 rounded">
