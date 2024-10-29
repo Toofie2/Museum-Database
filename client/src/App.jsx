2a4fd8e65aec702121fddb4730609abee8e0cd42
JosephOnwukwe <159090955+JosephOnwukwe@users.noreply.github.com.>
2024-10-29 05:09:28 -0500

    starting gift shop page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage.jsx"
 import TicketPage from "./pages/TicketPage.jsx"
 import ExhibitionsPage from './pages/ExhibitionsPage';
 import ExhibitionViewPage from './pages/ExhibitionViewPage';
+import GiftShopPage from "./pages/GiftShopPage";
 
 const App = () => {
   return (
@@ -12,7 +13,8 @@ const App = () => {
         <Route path="/home" element={<HomePage />} />
         <Route path="/tickets" element={<TicketPage />} />
         <Route path="/exhibitions" element={<ExhibitionsPage />} />
-      <Route path="/exhibition/:id" element={<ExhibitionViewPage />} />
+        <Route path="/exhibition/:id" element={<ExhibitionViewPage />} />
+        <Route path="/giftshop" element={<GiftShopPage />} />
     </Routes>
     </BrowserRouter>
   )
