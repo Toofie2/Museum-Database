c144e97feab739b985cf62aeeb54f44f09895f11
JosephOnwukwe <159090955+JosephOnwukwe@users.noreply.github.com.>
2024-10-29 02:06:39 -0500

    add gift shop background image

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const NavBar = () => {
         }
     ];
     return (
-        <nav className="fixed z-10 w-full text-black">
+        <nav className="fixed z-10 w-full text-white">
             <div className="flex justify-between items-center max-w-7xl mx-auto px-6 py-4">
                 <div className="w-1/2">
                     <ul className="flex justify-start gap-8">
@@ -54,7 +54,7 @@ const NavBar = () => {
                 </div>
                 <div className="flex justify-center">
                     <Link>
-                        <TheFAMLogo className="text-black" />
+                        <TheFAMLogo className="text-white" />
                     </Link>
                 </div>
                 <div className="flex justify-end w-1/2">
