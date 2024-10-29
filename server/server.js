d470b2cf9c132334344c7b8a18877a2c364f2ff3
JosephOnwukwe <159090955+JosephOnwukwe@users.noreply.github.com>
2024-10-29 11:35:50 -0500

    added product_category endpoint
    (cherry picked from commit c50b198e5248dd1d75fe4662a3005bd6586f6269)

--- server/server.js
+++ server/server.js
@@ -38,6 +38,9 @@ app.use('/customer_product', customerProductRouter);
 const productCategoryRouter = require('./routes/product_category');
 app.use('/product_category', productCategoryRouter);
 
+const productCategoryRouter = require('./routes/product_category');
+app.use('/product_category', productCategoryRouter);
+
 app.get("/", (req, res) => {
     res.json("Hello, this is the backend!");
 });
