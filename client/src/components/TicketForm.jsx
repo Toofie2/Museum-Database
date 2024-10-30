791c5fe2c4771b2ee8c1dca2604fd94aa59ed4fd
JosephOnwukwe <159090955+JosephOnwukwe@users.noreply.github.com.>
2024-10-29 21:03:01 -0500

    added a "thank you for your purchase page" that links bacl to home

--- client/src/components/TicketForm.jsx
+++ client/src/components/TicketForm.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 import DatePickerComponent from "./DatePickerComponent"
+import { Link } from 'react-router-dom'
 
 const TicketForm = () => {
     const[tickets, setTickets] = useState([]);
@@ -14,6 +15,7 @@ const TicketForm = () => {
         veteran: 0
     }); 
     
+    //This needs to be changed to the customer ID of the account making the purchase
     const customerID = 1;
 
     const{render, selectedDate} = DatePickerComponent();
@@ -59,6 +61,8 @@ const TicketForm = () => {
 
             await axios.post("http://localhost:3000/customer_ticket", purchasedTicketsArr);
 
+            
+
         }catch(err){
             console.log(err)
         }
@@ -90,16 +94,16 @@ const TicketForm = () => {
 
     return (
         <div className="ticketForm">
-            <h1 class="text-3xl font-medium">Purchase</h1>
-            <p class="mt-3 leading-loose">Please select an available date for your visit and the amount of tickets you’ll be purchasing.</p>
-            <div class="w-64 mt-5 py-2 px-5 border-2 border-black rounded">
+            <h1 className="text-3xl font-medium">Purchase</h1>
+            <p className="mt-3 leading-loose">Please select an available date for your visit and the amount of tickets you’ll be purchasing.</p>
+            <div className="w-64 mt-5 py-2 px-5 border-2 border-black rounded">
                 {render}
             </div>
-            <form class="mt-6">
+            <form className="mt-6">
                 <span >
-                <label class="flex justify-between">
+                <label className="flex justify-between">
                     <div>
-                    <span class="font-medium">Adult Admission (19+)</span> ${ticketPrices.at(0)}
+                    <span className="font-medium">Adult Admission (19+)</span> ${ticketPrices.at(0)}
                     </div>
                     <input 
                         type="number"
@@ -109,13 +113,13 @@ const TicketForm = () => {
                         placeholder="0"
                         value={formData.adult}
                         onChange={handleChange} 
-                        class="w-12"
+                        className="w-12"
                     />
                 </label>
                 <br></br>
-                <label class="flex justify-between">
+                <label className="flex justify-between">
                     <div>
-                    <span class="font-medium">Senior Admission (65+)</span>  ${ticketPrices.at(1)}
+                    <span className="font-medium">Senior Admission (65+)</span>  ${ticketPrices.at(1)}
                     </div>
                     <input 
                         type="number"
@@ -125,13 +129,13 @@ const TicketForm = () => {
                         placeholder="0"
                         value={formData.senior}
                         onChange={handleChange} 
-                        class="w-12"
+                        className="w-12"
                     />
                 </label>
                 <br></br>
-                <label class="flex justify-between">
+                <label className="flex justify-between">
                     <div>
-                <span class="font-medium">Youth Admission (13-18)</span>  ${ticketPrices.at(2)}
+                <span className="font-medium">Youth Admission (13-18)</span>  ${ticketPrices.at(2)}
                 </div>
                     <input 
                         type="number"
@@ -141,13 +145,13 @@ const TicketForm = () => {
                         placeholder="0"
                         value={formData.youth}
                         onChange={handleChange}
-                        class="w-12" 
+                        className="w-12" 
                     />
                 </label>
                 <br></br>
-                <label class="flex justify-between">
+                <label className="flex justify-between">
                     <div>
-                <span class="font-medium">Child Admission (12 & Under)</span>  ${ticketPrices.at(3)}
+                <span className="font-medium">Child Admission (12 & Under)</span>  ${ticketPrices.at(3)}
                 </div>
                     <input 
                         type="number"
@@ -157,13 +161,13 @@ const TicketForm = () => {
                         placeholder="0"
                         value={formData.child}
                         onChange={handleChange}
-                        class="w-12" 
+                        className="w-12" 
                     />
                 </label>
                 <br></br>
-                <label class="flex justify-between">
+                <label className="flex justify-between">
                     <div>
-                <span class="font-medium">Student Admission (with valid ID)</span>  ${ticketPrices.at(4)}
+                <span className="font-medium">Student Admission (with valid ID)</span>  ${ticketPrices.at(4)}
                 </div>
                     <input 
                         type="number"
@@ -173,13 +177,13 @@ const TicketForm = () => {
                         placeholder="0"
                         value={formData.student}
                         onChange={handleChange}
-                        class="w-12" 
+                        className="w-12" 
                     />
                 </label>
                 <br></br>
-                <label class="flex justify-between">
+                <label className="flex justify-between">
                     <div>
-                <span class="font-medium">Veteran Admission (with valid ID)</span>  ${ticketPrices.at(5)}
+                <span className="font-medium">Veteran Admission (with valid ID)</span>  ${ticketPrices.at(5)}
                 </div>
                     <input 
                         type="number"
@@ -189,16 +193,18 @@ const TicketForm = () => {
                         placeholder="0"
                         value={formData.veteran}
                         onChange={handleChange}
-                        class="w-12" 
+                        className="w-12" 
                     />
                 </label>
                 </span>
-                <div class="mt-10 text-default-gray text-lg flex justify-between">
-                    <span class="font-medium">Subtotal</span>${subtotal}
+                <div className="mt-10 text-default-gray text-lg flex justify-between">
+                    <span className="font-medium">Subtotal</span>${subtotal}
                 </div>
-                <button class="w-full mt-4 bg-black text-white py-2 px-52 border-black rounded" onClick={handleSubmit}>
+                <Link to={'/tickets/purchased'}>
+                <button className="w-full mt-4 bg-black text-white py-2 px-52 border-black rounded" onClick={handleSubmit}>
                     Purchase
                 </button>   
+                </Link>
             </form>
         </div>
     );
