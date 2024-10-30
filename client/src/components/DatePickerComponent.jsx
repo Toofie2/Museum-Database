a95027e4b44a6a0842c1916730f491ac0bd33747
MariaVarghese1010 <mariavarghese1010@gmail.com>
2024-10-30 01:36:21 -0500

    a bunch more edits and front end stuff for collections

---
+++ client/src/components/DatePickerComponent.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
+
+const DatePickerComponent = () =>{
+
+    function addMonths(date, months) {
+        let temp = new Date(date)
+        temp.setMonth(temp.getMonth() + months)
+        if (temp.getDate() != date.getDate()) temp.setDate(0)
+        return temp
+      }
+
+    const [selectedDate, setSelectedDate] = useState(new Date())
+    const minDate = new Date();
+    const maxDate = addMonths(minDate, 3);
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
+
+
+    return{
+        selectedDate,
+        render:(
+        <div>
+            <DatePicker 
+            selected={selectedDate}
+            onChange={handleDateChange}
+            dateFormat="MMMM dd, yyyy"
+            minDate={minDate}
+            maxDate={maxDate}
+            />
+        </div>
+        )
+    }
+}
+
+export default DatePickerComponent;
