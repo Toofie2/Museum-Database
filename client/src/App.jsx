870166c4ff14c775199379cf96457fdedb0f19ab
Isaias Gonzalez <xisaiasgonzalez@gmail.com>
2024-10-30 01:38:10 -0500

    Fixed App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import HomePage from "./pages/HomePage.jsx"
-import TicketPage from "./pages/TicketPage.jsx"
-import ExhibitionsPage from './pages/ExhibitionsPage';
-import ExhibitionViewPage from './pages/ExhibitionViewPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import TicketPage from "./pages/TicketPage.jsx";
+import ExhibitionsPage from "./pages/ExhibitionsPage";
+import ExhibitionViewPage from "./pages/ExhibitionViewPage";
 import GiftShopPage from "./pages/GiftShopPage";
 import GiftShopCategoryPage from "./pages/GiftShopCategoryPage";
 import TicketPurchasedPage from "./pages/TicketPurchasedPage.jsx";
@@ -12,16 +11,16 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/tickets" element={<TicketPage />} />
         <Route path="/tickets/purchased" element={<TicketPurchasedPage />} />
         <Route path="/exhibitions" element={<ExhibitionsPage />} />
         <Route path="/exhibition/:id" element={<ExhibitionViewPage />} />
-        <Route path="/giftshop" element={<GiftShopPage />} />
+        <Route path="/shop" element={<GiftShopPage />} />
         <Route path="/giftshop/:id" element={<GiftShopCategoryPage />} />
-    </Routes>
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
