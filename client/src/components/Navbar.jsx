b08a8f497e572192454155cec6b7f4ac15fc3d8d
MariaVarghese1010 <mariavarghese1010@gmail.com>
2024-10-30 01:36:21 -0500

    a bunch more edits and front end stuff for collections

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,101 @@
+import { Link } from "react-router-dom";
+import { TheFAMLogo } from "../constants/components.jsx";
+import { useState, useEffect } from "react";
+
+const NavBar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const leftnavigation = [
+    {
+      name: "Tickets",
+      link: "/tickets",
+    },
+    {
+      name: "Membership",
+      link: "/membership",
+    },
+    {
+      name: "Exhibitions",
+      link: "/exhibitions",
+    },
+    {
+      name: "Art",
+      link: "/art",
+    },{
+        name: "Collections",
+        link: "/collections",
+      },
+    {
+      name: "Shop",
+      link: "/shop",
+    },
+    {
+      name: "Review",
+      link: "/review",
+    },
+  ];
+  const rightnavigation = [
+    {
+      name: "Login",
+      link: "/login",
+    },
+    {
+      name: "Cart (0)",
+      link: "/cart",
+    },
+  ];
+  return (
+    <nav
+      className={`fixed z-10 w-full transition-all duration-300 ${
+        isScrolled ? "bg-white [&_svg]:text-black shadow-md" : "text-white"
+      }`}
+    >
+      <div className="flex justify-between items-center max-w-full mx-auto px-16 py-4">
+        <div className="w-1/2">
+          <ul className="flex justify-start gap-8">
+            {leftnavigation.map((item) => (
+              <li key={item.name}>
+                <Link
+                  className="text-light-grey hover:text-white cursor-pointer"
+                  to={item.link}
+                >
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex justify-center">
+          <Link to="/">
+            <TheFAMLogo />
+          </Link>
+        </div>
+        <div className="flex justify-end w-1/2">
+          <ul className="flex justify-start gap-8">
+            {rightnavigation.map((item) => (
+              <li key={item.name}>
+                <Link to={item.link}>{item.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ client/src/components/TicketInfo.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const TicketInfo = () => {
+    return(
+       <div>
+            <h1 className="text-3xl font-medium">
+                Admission Tickets
+            </h1>
+            <p className="leading-loose mt-3">
+                $24 for adults; $20 for seniors; $15 for youth, $17 for students; <br></br>
+                Free for Members, children under 12, and veterans. <br></br> 
+                Must verify student or veteran status at entry with a valid ID <br></br>
+                All tickets give access to same-day entry to museum for the date on your ticket.
+            </p>
+            <div className="text-white box-border h-auto w-full px-5 py-4 border-4 border-gray-brown rounded bg-gray-brown mt-6">
+                <h2 className="text-xl font-medium">
+                    Become a Member
+                </h2>
+                <p className="mt-1">
+                    Enjoy <span className="font-medium">unlimited free admission</span> for you and your guest(s) on every visit!
+                </p>
+                
+                <Link to={'/membership'}>
+                    <button className=" mt-5 text-white py-2 px-5 border-2 border-white rounded">
+                        Join now
+                    </button>
+                </Link>
+            </div>
+       </div> 
+    )
+}
+
+
+
+
+export default TicketInfo
