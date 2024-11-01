4ec019eebd2786717fb8768f027494c92daeab65
Isaias Gonzalez <xisaiasgonzalez@gmail.com>
2024-11-01 18:53:54 -0500

    Fixed cors backend

--- server/server.js
+++ server/server.js
@@ -1,9 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 //import cors from 'cors'
 
 const app = express();
+
+const allowedOrigins = ["https://museum-database-project.vercel.app"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
 app.use(cors());
 const port = process.env.PORT;
 
@@ -12,55 +26,55 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // Backend routes for each table
-const employeeRouter = require('./routes/employee');
-app.use('/employee', employeeRouter);
+const employeeRouter = require("./routes/employee");
+app.use("/employee", employeeRouter);
 
-const customerRouter = require('./routes/customer');
-app.use('/customer', customerRouter);
+const customerRouter = require("./routes/customer");
+app.use("/customer", customerRouter);
 
-const authenticationRouter = require('./routes/authentication');
-app.use('/authentication', authenticationRouter);
+const authenticationRouter = require("./routes/authentication");
+app.use("/authentication", authenticationRouter);
 
-const customer_exhibitionRouter = require('./routes/customer_exhibition');
-app.use('/customer_exhibition', customer_exhibitionRouter);
+const customer_exhibitionRouter = require("./routes/customer_exhibition");
+app.use("/customer_exhibition", customer_exhibitionRouter);
 
-const roomRouter = require('./routes/room');
-app.use('/room', roomRouter);
+const roomRouter = require("./routes/room");
+app.use("/room", roomRouter);
 
-const exhibitionRouter = require('./routes/exhibition');
-app.use('/exhibition', exhibitionRouter);
+const exhibitionRouter = require("./routes/exhibition");
+app.use("/exhibition", exhibitionRouter);
 
-const ticketRouter = require('./routes/ticket');
-app.use('/ticket', ticketRouter);
+const ticketRouter = require("./routes/ticket");
+app.use("/ticket", ticketRouter);
 
-const customerTicketRouter = require('./routes/customer_ticket');
-app.use('/customer_ticket', customerTicketRouter);
+const customerTicketRouter = require("./routes/customer_ticket");
+app.use("/customer_ticket", customerTicketRouter);
 
-const productRouter = require('./routes/product');
-app.use('/product', productRouter);
+const productRouter = require("./routes/product");
+app.use("/product", productRouter);
 
-const customerProductRouter = require('./routes/customer_product');
-app.use('/customer_product', customerProductRouter);
+const customerProductRouter = require("./routes/customer_product");
+app.use("/customer_product", customerProductRouter);
 
-const productCategoryRouter = require('./routes/product_category');
-app.use('/product_category', productCategoryRouter);
+const productCategoryRouter = require("./routes/product_category");
+app.use("/product_category", productCategoryRouter);
 
-const reviewRouter = require('./routes/review');
-app.use('/review', reviewRouter);
+const reviewRouter = require("./routes/review");
+app.use("/review", reviewRouter);
 
-const artRouter = require('./routes/art');
-app.use('/art', artRouter);
+const artRouter = require("./routes/art");
+app.use("/art", artRouter);
 
-const artistRouter = require('./routes/artist');
-app.use('/artist', artistRouter);
+const artistRouter = require("./routes/artist");
+app.use("/artist", artistRouter);
 
-const collectionRouter = require('./routes/collection');
-app.use('/collection', collectionRouter);
+const collectionRouter = require("./routes/collection");
+app.use("/collection", collectionRouter);
 
 app.get("/", (req, res) => {
-    res.json("Hello, this is the backend!");
+  res.json("Hello, this is the backend!");
 });
 
 app.listen(port, () => {
-    console.log(`Connected to backend on port ${port}`);
+  console.log(`Connected to backend on port ${port}`);
 });
