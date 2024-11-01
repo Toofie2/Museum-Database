2a704690eced70429f35675a919ad984906c8cba
Isaias Gonzalez <xisaiasgonzalez@gmail.com>
2024-11-01 18:16:23 -0500

    Updated axios URLs to backend URL

--- client/src/pages/TicketPage.jsx
+++ client/src/pages/TicketPage.jsx
@@ -1,25 +1,22 @@
-import React from 'react'
-import TicketInfo from "../components/TicketInfo.jsx"
-import TicketForm from "../components/TicketForm.jsx"
+import React from "react";
+import TicketInfo from "../components/TicketInfo.jsx";
+import TicketForm from "../components/TicketForm.jsx";
 import Navbar from "../components/Navbar.jsx";
 
-
 const TicketPage = () => {
-    return(
-        <div className="relative min-h-screen p-1">
-            <Navbar/>
-            <div className="mt-28 flex justify-between px-16 space-x-24">
-                <div>
-                    <TicketInfo />
-                </div>
-                <div>
-                    <TicketForm />
-                </div>
-            </div>
+  return (
+    <div className="relative min-h-screen p-1">
+      <Navbar />
+      <div className="mt-28 flex justify-between px-16 space-x-24">
+        <div>
+          <TicketInfo />
         </div>
-    )
+        <div>
+          <TicketForm />
+        </div>
+      </div>
+    </div>
+  );
 };
-
-
 
 export default TicketPage;
