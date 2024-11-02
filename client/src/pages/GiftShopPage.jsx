de9f73edf12063682de5d0eb6a4dc54bf37f4f15
JosephOnwukwe <159090955+JosephOnwukwe@users.noreply.github.com.>
2024-11-02 16:05:46 -0500

    modified gift shop page to fit screen. removed images stored in assets.

--- client/src/pages/GiftShopPage.jsx
+++ client/src/pages/GiftShopPage.jsx
@@ -22,16 +22,16 @@ const GiftShopPage = () => {
   }, []);
 
   return (
-    <div className="container mx-auto p-0">
+    <div className="mx-auto p-0">
       <Navbar />
       {/* Banner Section */}
-      <div className="relative flex items-center h-[800px]">
+      <div className="relative flex items-center h-[800px] w-screen">
         <img
           src={backgroundImage}
           alt="Gift Shop Banner"
-          className="w-full h-full object-cover absolute top-0 left-0"
+          className="object-cover absolute top-0 left-0 h-full w-screen"
         />
-        <div className="absolute inset-0 bg-black bg-opacity-40 flex flex-col justify-center pl-4 space-y-2">
+        <div className="absolute inset-0 bg-black bg-opacity-40 flex flex-col justify-center pl-10 space-y-2">
           <h1 className="text-8xl font-regular text-white z-10 w-[600px] p-6">
             THE GIFT <br /> SHOP
           </h1>
@@ -44,13 +44,13 @@ const GiftShopPage = () => {
       </div>
 
       {/* Product Category List */}
-      <div className="container mx-auto p-7 pb-20">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7">
+      <div className="container mx-auto py-7 pb-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7 w-auto px-5">
           {productCategories.map((PC) => {
             return (
               <div
                 key={PC.product_category_id}
-                className="exhibition-item relative bg-white rounded-lg overflow-hidden shadow-lg transition-transform duration-300 transform hover:scale-105"
+                className="gift-shop-category-item relative bg-white rounded-lg overflow-hidden shadow-lg transition-transform duration-300 transform hover:scale-105 w-auto"
               >
                 <Link to={`/giftshop/${PC.product_category_id}`}>
                   <img
