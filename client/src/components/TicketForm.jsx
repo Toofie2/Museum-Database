4b7bc83c00d322507c763d99b8c0bfdedd781843
JosephOnwukwe <159090955+JosephOnwukwe@users.noreply.github.com.>
2024-11-06 16:54:40 -0600

    implementing add and subtract icons

--- client/src/components/TicketForm.jsx
+++ client/src/components/TicketForm.jsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from 'react'
 import axios from 'axios'
 import DatePickerComponent from "./DatePickerComponent"
 import { Link } from 'react-router-dom'
+import AddIcon from './AddIcon.jsx'
 
 const TicketForm = () => {
   const [tickets, setTickets] = useState([]);
@@ -157,118 +158,123 @@ const TicketForm = () => {
         Please select an available date for your visit and the amount of tickets
         you’ll be purchasing.
       </p>
-      <div className="w-64 mt-5 py-2 px-5 border-2 border-black rounded">
+      <div className="w-60 mt-5 py-2 pl-3 border-2 border-black rounded">
         {render}
       </div>
-      <form className="mt-6 grid grid-cols-1 divide-y-2">
-        <div className="flex justify-between">
-          <label htmlFor="adult">
-            <span className="font-medium">Adult Admission (19+)</span> ${ticketPrices.at(0)}
-          </label>
-          <div>
-            <button name="adult" value={formData.adult} onClick={handleDecrease}>-</button>
-            <input
-              type="number"
-              name="adult"
-              id="adult"
-              min="0"
-              max="20"
-              placeholder="0"
-              value={formData.adult}
-              onChange={handleChange}
-              className="w-12 text-center"
-            />
-            <button name="adult" value={formData.adult} onClick={handleIncrease}>+</button>
+      <form className="mt-6">
+        <div className="grid grid-cols-1 divide-y-2">
+          <div></div>
+          <div className="flex justify-between pt-5 pb-5">
+            <label htmlFor="adult">
+              <span className="font-medium">Adult Admission (19+)</span> ${ticketPrices.at(0)}
+            </label>
+            <div className="flex flex-row">
+              <input
+                type="number"
+                name="adult"
+                id="adult"
+                min="0"
+                max="20"
+                placeholder="0"
+                value={formData.adult}
+                onChange={handleChange}
+                className="w-12 text-center"
+              />
+              <AddIcon/>
+            </div>
           </div>
+          <div className="flex justify-between pt-5 pb-5">
+            <label htmlFor="senior">
+              <span className="font-medium">Senior Admission (65+)</span> ${ticketPrices.at(1)}
+            </label>
+            <div>
+              <input
+                type="number"
+                name="senior"
+                id="senior"
+                min="0"
+                max="20"
+                placeholder="0"
+                value={formData.senior}
+                onChange={handleChange}
+                className="w-12 text-center"
+              />
+            </div>
+          </div>
+          <div className="flex justify-between pt-5 pb-5">
+            <label htmlFor="youth">
+              <span className="font-medium">Youth Admission (13-18)</span> ${ticketPrices.at(2)}
+            </label>
+            <div>
+              <input
+                type="number"
+                name="youth"
+                id="youth"
+                min="0"
+                max="20"
+                placeholder="0"
+                value={formData.youth}
+                onChange={handleChange}
+                className="w-12 text-center"
+              />
+            </div>
+          </div>
+          <div className="flex justify-between pt-5 pb-5">
+            <label htmlFor="youth">
+              <span className="font-medium">Child Admission (12 & Under)</span> ${ticketPrices.at(3)}
+            </label>
+            <div>
+              <input
+                type="number"
+                name="child"
+                id="child"
+                min="0"
+                max="20"
+                placeholder="0"
+                value={formData.child}
+                onChange={handleChange}
+                className="w-12 text-center"
+              />
+            </div>
+          </div>
+          <div className="flex justify-between pt-5 pb-5">
+            <label htmlFor="youth">
+              <span className="font-medium">Student Admission (with valid ID)</span> ${ticketPrices.at(4)}
+            </label>
+            <div>
+              <input
+                type="number"
+                name="student"
+                id="student"
+                min="0"
+                max="20"
+                placeholder="0"
+                value={formData.student}
+                onChange={handleChange}
+                className="w-12 text-center"
+              />
+            </div>
+          </div>
+          <div className="flex justify-between pt-5 pb-5">
+            <label htmlFor="youth">
+              <span className="font-medium">Veteran Admission (with valid ID)</span> ${ticketPrices.at(5)}
+            </label>
+            <div>
+              <input
+                type="number"
+                name="veteran"
+                id="veteran"
+                min="0"
+                max="20"
+                placeholder="0"
+                value={formData.veteran}
+                onChange={handleChange}
+                className="w-12 text-center"
+              />
+            </div>
+          </div>
+          <div></div>
         </div>
-        <label className="flex justify-between">
-          <div>
-            <span className="font-medium">Senior Admission (65+)</span> $
-            {ticketPrices.at(1)}
-          </div>
-          <input
-            type="number"
-            name="senior"
-            min="0"
-            max="50"
-            placeholder="0"
-            value={formData.senior}
-            onChange={handleChange}
-            className="w-12"
-          />
-        </label>
-        <br></br>
-        <label className="flex justify-between">
-          <div>
-            <span className="font-medium">Youth Admission (13-18)</span> $
-            {ticketPrices.at(2)}
-          </div>
-          <input
-            type="number"
-            name="youth"
-            min="0"
-            max="50"
-            placeholder="0"
-            value={formData.youth}
-            onChange={handleChange}
-            className="w-12"
-          />
-        </label>
-        <br></br>
-        <label className="flex justify-between">
-          <div>
-            <span className="font-medium">Child Admission (12 & Under)</span>{" "}
-            ${ticketPrices.at(3)}
-          </div>
-          <input
-            type="number"
-            name="child"
-            min="0"
-            max="50"
-            placeholder="0"
-            value={formData.child}
-            onChange={handleChange}
-            className="w-12"
-          />
-        </label>
-        <br></br>
-        <label className="flex justify-between">
-          <div>
-            <span className="font-medium">
-              Student Admission (with valid ID)
-            </span>{" "}
-            ${ticketPrices.at(4)}
-          </div>
-          <input
-            type="number"
-            name="student"
-            min="0"
-            max="50"
-            placeholder="0"
-            value={formData.student}
-            onChange={handleChange}
-            className="w-12"
-          />
-        </label>
-        <br></br>
-        <label className="flex justify-between">
-          <div>
-            <span className="font-medium">
-              Veteran Admission (with valid ID)
-            </span>{" "}
-            ${ticketPrices.at(5)}
-          </div>
-          <input
-            type="number"
-            name="veteran"
-            min="0"
-            max="50"
-            placeholder="0"
-            value={formData.veteran}
-            onChange={handleChange}
-            className="w-12"
-          />
-        </label>
         <div className="mt-10 text-default-gray text-lg flex justify-between">
           <span className="font-medium">Subtotal</span>${subtotal}
         </div>
